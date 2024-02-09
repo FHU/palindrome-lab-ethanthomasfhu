b64802e6764e091fcac9d0ee1f9db16eb5f44768
Ethan Thomas <ethan.thomas@students.fhu.edu>
2024-02-09 17:17:11 -0600

    Update main.py
    Fixed.

--- main.py
+++ main.py
@@ -2,15 +2,11 @@ def palindrome(word):
      word = word.replace(" ", "").lower()
      return word == word[::-1]
 
-def main():
+if __name__ == '__main__': 
     user_input = input("Enter text: ")
     if palindrome(user_input):
         print("True")
     else:
         print("False")
 
-
-
-if __name__ == '__main__': 
-    main()
 
